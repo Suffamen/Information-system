39ed66c8584a1deec1df8021f73c9014854433a8
kurumoch <denis_kurumoch@mail.ru>
2016-11-04 16:50:43 +0400

    Added listeners

--- InformationSystem/src/informationsystem/views/AddNoteView.java
+++ InformationSystem/src/informationsystem/views/AddNoteView.java
@@ -1,6 +1,8 @@
 package informationsystem.views;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by Павлов on 04.11.2016.
@@ -14,4 +16,13 @@ public class AddNoteView {
     private JLabel rightSpacer;
     private JLabel bottomSpacer;
     private JLabel topSpacer;
+
+    public AddNoteView() {
+        createButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                
+            }
+        });
+    }
 }
--- InformationSystem/src/informationsystem/views/AddNotebookView.java
+++ InformationSystem/src/informationsystem/views/AddNotebookView.java
@@ -1,6 +1,8 @@
 package informationsystem.views;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by Павлов on 04.11.2016.
@@ -13,4 +15,13 @@ public class AddNotebookView {
     private JLabel LeftSpacer;
     private JLabel topSpacer;
     private JButton createButton;
+
+    public AddNotebookView() {
+        createButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
 }
